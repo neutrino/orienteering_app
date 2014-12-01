a8e1ac681409f4dc24d67e75f0a279111ff6d699
ranendra <neutrino@ranendra.com.np>
2014-12-01 23:19:59 +0200

    Skipped verification of authenticity token for JSON requests.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,9 +1,19 @@
 class Api::BaseController < ApplicationController
-  respond_to :json
 
+  protect_from_forgery
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  respond_to :json
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  protected
 
   def not_found
     render json: { error: "Not Found"}, status: 404
   end
+
+  def json_request?
+    request.format.json?
+  end
+
 end
