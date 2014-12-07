95ad58ccc0d6fbe1aad378cbebaccae7407c6566
ranendra <neutrino@ranendra.com.np>
2014-12-07 05:04:18 +0200

    Added an active scope in Event.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ActiveRecord::Base
   has_many :tracks
   validates :name, :location, :start_date, :end_date, :starting_point, presence: true
 
+  scope :active, -> { where("? BETWEEN start_date AND end_date", Date.today) }
+
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -14,4 +14,19 @@ describe Event do
 
   it { should respond_to(:start_date) }
 
+  context ".active" do
+    before(:each) do
+      @event_one = FactoryGirl.create(:event,
+        start_date: Time.now - 5.days,
+        end_date: Time.now - 2.days)
+      @event_two = FactoryGirl.create(:event,
+        start_date: Time.now - 5.days,
+        end_date: Time.now + 1.days)
+    end
+    it "returns the events that are currently active" do
+      expect(Event.active).to include @event_two
+      expect(Event.active.count).to be 1
+    end
+  end
+
 end
