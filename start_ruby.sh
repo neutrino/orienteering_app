afff4590b08fd248a5d69fe49f3e0a7272b38ee0
Snurppa <snurppa@gmail.com>
2014-12-21 23:13:00 +0200

    skip documents with gem

--- start_ruby.sh
+++ start_ruby.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "install: --no-document" > ~/.gemrc
+echo "update: --no-document" >> ~/.gemrc
+
 cd /vagrant
 cp config/database.example.yml config/database.yml
 cp config/secrets.example.yml config/secrets.yml
