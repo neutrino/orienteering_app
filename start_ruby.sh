9f5d62cc62f53b84e19841f041e1fe4656c7ddf8
Snurppa <snurppa@gmail.com>
2014-12-22 00:05:34 +0200

    don't drop stuff from db without asking...

--- start_ruby.sh
+++ start_ruby.sh
@@ -8,7 +8,7 @@ cp config/database.example.yml config/database.yml
 cp config/secrets.example.yml config/secrets.yml
 bundle
 echo "Creating databases, migrations and seeds. Admin user is added based on secrets.yml.."
-bundle exec rake db:drop db:create db:migrate db:seed
+bundle exec rake db:create db:migrate db:seed
 echo "done."
 rails runner "puts 'Evoke rails runner...'"
 echo "Starting rails server..."
