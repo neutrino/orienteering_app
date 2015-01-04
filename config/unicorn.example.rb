8ca2553ccc44c02f76a554369d95c0c5fc62e697
Snurppa <snurppa@gmail.com>
2015-01-04 20:25:15 +0200

    add active record checks

--- config/unicorn.example.rb
+++ config/unicorn.example.rb
@@ -9,6 +9,19 @@ listen "/tmp/unicorn.orienteering.sock"
 worker_processes 3
 timeout 30
 
+# If using ActiveRecord, disconnect (from the database) before forking.
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+end
+
+# After forking, restore your ActiveRecord connection.
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
+end
+
+
 # Force the bundler gemfile environment variable to
 # reference the capistrano "current" symlink
 before_exec do |_|
