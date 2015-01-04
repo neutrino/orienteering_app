985027c68d5202a5774b04a954446dbe6eb593e0
Snurppa <snurppa@gmail.com>
2015-01-04 15:53:55 +0200

    refresh bashrc so rails is known

--- start_ruby.sh
+++ start_ruby.sh
@@ -7,9 +7,11 @@ cd /vagrant
 cp config/database.example.yml config/database.yml
 cp config/secrets.example.yml config/secrets.yml
 bundle
+echo "Refreshing $PATH..."
+. ~/.bashrc
+echo "done."
 echo "Creating databases, migrations and seeds. Admin user is added based on secrets.yml.."
 bundle exec rake db:create db:migrate db:seed
 echo "done."
-rails runner "puts 'Evoke rails runner...'"
 echo "Starting rails server..."
 rails server
